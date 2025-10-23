f7b5d68b74ea7d522257f54250fb7f43077b3d59
FIKY 0911 <fiky0911@gmail.com>
2025-10-23 22:59:42 +0700

    revert: Remove custom dashboard widgets and related queries

--- sanity/plugins/dashboard.ts
+++ sanity/plugins/dashboard.ts
@@ -1,17 +1,26 @@
-"use client"
-
 import { definePlugin } from "sanity";
-import { dashboardTool } from "@sanity/dashboard";
-import { TotalRevenueWidget, TotalProductCountWidget, TotalBlogCountWidget } from './custom-widgets';
+import { dashboardTool, projectInfoWidget, projectUsersWidget } from "@sanity/dashboard";
 
 export const customDashboard = definePlugin({
   name: "custom-dashboard",
   plugins: [
     dashboardTool({
       widgets: [
-        TotalRevenueWidget(),
-        TotalProductCountWidget(),
-        TotalBlogCountWidget(),
+        projectInfoWidget({
+          __experimental_before: [],
+          data: [
+            {
+              title: "GitHub Repo",
+              value: "https://github.com/FIKY0911/Grocerystore", // TODO: Update with actual GitHub repo URL
+              category: "Code",
+            },
+            {n              title: "Frontend URL",
+              value: "https://grocerystore.vercel.app", // TODO: Update with actual frontend URL
+              category: "apps",
+            },
+          ],
+        }),
+        projectUsersWidget({ title: 'Project members' }),
       ],
     }),
   ],
