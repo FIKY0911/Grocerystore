9de4ddfa6124e56a74eb3f53f308ff3c5b1a8316
FIKY 0911 <fiky0911@gmail.com>
2025-11-09 11:15:06 +0700

    Fix Logo

--- app/(client)/shop/page.tsx
+++ app/(client)/shop/page.tsx
@@ -1,13 +1,27 @@
+"use client";
+
+import React, { Suspense } from "react";
 import Shop from "@/components/Shop";
 import { getAllBrands, getCategories } from "@/sanity/queries";
-import React from "react";
 
-const ShopPage = async () => {
-  const categories = await getCategories();
-  const brands = await getAllBrands();
+const ShopPage = () => {
+  const [categories, setCategories] = React.useState([]);
+  const [brands, setBrands] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const [cat, br] = await Promise.all([getCategories(), getAllBrands()]);
+      setCategories(cat);
+      setBrands(br);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="bg-white">
-      <Shop categories={categories} brands={brands} />
+      <Suspense fallback={<div className="p-10 text-center">Loading Shop...</div>}>
+        <Shop categories={categories} brands={brands} />
+      </Suspense>
     </div>
   );
 };
